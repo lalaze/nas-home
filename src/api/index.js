9fd99f19406e152ce6b5b801618acd70f43c3d0f
lalaze <lalaze1996@gmail.com>
2021-12-17 19:25:42 +0800

    🐛 serverless接通

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,14 @@
-
-const {setIcon, getAllIcon, updateIcon} = require('./api')
+const api = require('./api')
 
 const main_handler = async (event, context) => {
-    const name = event.name; 
-    const res = await getAllIcon()
-    return res
+    const { method, data } = event.queryString
+    const res = await api[method](data)
+    return {
+        "isBase64Encoded": false,
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": JSON.stringify({'result': res})
+    }
 };
 
 module.exports =  { main_handler }
